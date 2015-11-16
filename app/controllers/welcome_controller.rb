ccb02db75704e3840f7a7a5444aecbac4e78efde
RichardMShay <rshay@mac.com>
2015-11-16 16:50:36 +0000

    activate menu items

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def about
+  end
+
+  def index
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+end
